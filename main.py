a367a7722e975e3306a8e0190193873cc3ced7b9
Agustin Mansilla <mansillaagustin1019@gmail.com>
2023-06-30 21:05:13 -0300

    Ordenar codigo

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from requests.auth import HTTPProxyAuth
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -9,14 +10,20 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 from random import randint
 from selenium.webdriver.firefox.options import Options as CustomFireFoxOptions
-from fake_headers import Headers
 from bs4 import BeautifulSoup
 import json
 
 
-
-
 def main():
+    driver = iniciar_driver()
+
+    html_elementos = extraer_tweets(driver)
+
+    resultados = extraer_informacion_tweets(html_elementos)
+
+    guardar_resultados(resultados)
+
+def iniciar_driver():
     browser_option = CustomFireFoxOptions()
     browser_option.add_argument('--profile=/home/agustin/.mozilla/firefox/tgx4az00.default-release')
 
@@ -25,21 +32,21 @@ def main():
     # Abrir Twitter
     driver.get("https://twitter.com/elonmusk")
 
-    # Espera 20 segundos
-    time.sleep(20)
+    return driver
 
-    # Presionar enter
-    body = driver.find_element(By.CSS_SELECTOR, 'body')
-    body.send_keys(Keys.ENTER)
+def extraer_tweets(driver):
+    # Definir la distancia del scroll automatico
+    altura = 400
     
+    # Extraer los primeros tweets
     all_ready_fetched_posts = []
     present_tweets = driver.find_elements(By.CSS_SELECTOR, '[data-testid="tweet"]')
     all_ready_fetched_posts.extend(present_tweets)
-    altura = 400
     html_elementos = []
     while len(html_elementos) < 10:
         for tweet in present_tweets:
             try:
+                # Almacenar el contenido HTML
                 elemento = tweet.get_attribute('innerHTML')
                 html_elementos.append(elemento)
             except Exception as e:
@@ -48,16 +55,21 @@ def main():
         # Scroll pagina
         driver.execute_script(f"window.scrollTo(0, {altura});")
         altura += 400
+
         # Realizar una espera
         wait_until_completion(driver)
         wait_until_tweets_appear(driver)
+
         # Seleccionar todos los twits que encuentra
         present_tweets = driver.find_elements(By.CSS_SELECTOR, '[data-testid="tweet"]')
+
         # Guardar unicamente lo twits nuevos
         present_tweets = [post for post in present_tweets if post not in all_ready_fetched_posts]
         all_ready_fetched_posts.extend(present_tweets)
         print(f"Twets Encontrados: {len(html_elementos)}")
+    return html_elementos
 
+def extraer_informacion_tweets(html_elementos):
     resultados = []
     for elemento in html_elementos:
         resultado = {}
@@ -71,16 +83,19 @@ def main():
             for cabecera in cabeceras:
                 etiquetas_a = cabecera.find_all('a')
                 if len(etiquetas_a) > 0:
+                    # Extraer Nombre Perfil
                     nombre_usuario = etiquetas_a[0].find_all('span')
                     if len(nombre_usuario) > 0:
                         print(f"Nombre: {nombre_usuario[0].text.strip()}")
                         resultado['nombre'] = nombre_usuario[0].text.strip()
-
+                    
+                    # Extraer usuario
                     usuario = etiquetas_a[1].find_all('span')
                     if len(usuario) > 0:
                         print(f"Usuario: {usuario[-1].text.strip()}")
                         resultado['usuario'] = usuario[-1].text.strip()
 
+                    # Extraer fecha publicacion
                     for etiqueta_a in etiquetas_a:
                         et_time = etiqueta_a.find_all('time')
                         if len(et_time) > 0:
@@ -133,35 +148,11 @@ def main():
 
             print("---------------------")
         resultados.append(resultado)
+    return resultados
+
+def guardar_resultados(resultados):
     with open("resultados.json","w") as file:
         json.dump(resultados,file)
-
-
-def set_properties(browser_option):
-    """adds capabilities to the driver"""
-    header = Headers().generate()['User-Agent']
-    browser_option.add_argument('--no-sandbox')
-    browser_option.add_argument("--disable-dev-shm-usage")
-    browser_option.add_argument('--ignore-certificate-errors')
-    browser_option.add_argument('--disable-gpu')
-    browser_option.add_argument('--log-level=3')
-    browser_option.add_argument('--disable-notifications')
-    browser_option.add_argument('--disable-popup-blocking')
-    browser_option.add_argument('--user-agent={}'.format(header))
-    return browser_option
-
-
-def scroll_down(driver) -> None:
-    """Helps to scroll down web page"""
-    try:
-        driver.execute_script("window.scrollTo(0, 500);")
-        # body = driver.find_element(By.CSS_SELECTOR, 'body')
-        # # for _ in range(randint(1, 3)):
-        # for _ in range(2):
-        #     body.send_keys(Keys.PAGE_DOWN)
-        #     time.sleep(3)
-    except Exception as ex:
-        print("Error at scroll_down method {}".format(ex))
 
 def wait_until_tweets_appear(driver) -> None:
     """Wait for tweet to appear. Helpful to work with the system facing
@@ -182,16 +173,6 @@ def wait_until_completion(driver) -> None:
             state = driver.execute_script("return document.readyState")
     except Exception as ex:
         print('Error at wait_until_completion: {}'.format(ex))
-
-def find_status(tweet):
-    """finds status and link from the tweet"""
-    try:
-        anchor = tweet.find_element(
-            By.CSS_SELECTOR, "a[aria-label][dir]")
-        return (anchor.get_attribute("href").split("/"), anchor.get_attribute("href"))
-    except Exception as ex:
-        print("Error at method find_status : {}".format(ex))
-        return [0],""
 
 
 main()
